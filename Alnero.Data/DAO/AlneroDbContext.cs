a74ecd2cf823969a88025b9da9860bf3a0453472
flyhu <flyhu@FLYHUB>
2024-03-29 01:53:53 +0400

    Add project files.

---
+++ Alnero.Data/DAO/AlneroDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Alnero.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alnero.Data.DAO
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<OrderEntity> order {  get; set; }
+
+    }
+}
---
+++ Alnero.Data/Entities/OrderEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alnero.Data.Entities
+{
+    public class OrderEntity
+    {
+        private string firstName;
+
+        public  int Id { get; set; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+       
+        public string StreetAddress { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
