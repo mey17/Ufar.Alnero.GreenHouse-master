65e26b0dc6c11d0614fc50c5a90fb2a91d461a3e
flyhu <flyhu@FLYHUB>
2024-04-05 09:12:01 +0400

    Updated Order and Technical with mr. Yeprikyan:)

--- Alnero.Data/Entities/OrderEntity.cs
+++ Alnero.Data/Entities/OrderEntity.cs
@@ -6,20 +6,35 @@ namespace Alnero.Data.Entities
     {
         private string firstName;
 
-        public  int Id { get; set; }
-        public string FirstName { get => firstName; set => firstName = value; }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "First name must be between 1 and 50 characters")]
+        [RegularExpression(@"^[A-Za-z]*$", ErrorMessage = "First name can only contain letters")]
+        public string FirstName
+        {
+            get => firstName;
+            set => firstName = value?.Trim(); // Trim leading and trailing whitespace
+        }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Last name must be between 1 and 50 characters")]
+        [RegularExpression(@"^[A-Za-z]*$", ErrorMessage = "Last name can only contain letters")]
         public string LastName { get; set; }
 
-        [DataType(DataType.PhoneNumber)]
-        [Required]
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "Phone number must be 8 digits")]
         public string PhoneNumber { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
+
         public string Province { get; set; }
         public string City { get; set; }
-       
         public string StreetAddress { get; set; }
 
+        [RegularExpression(@"^\d{4}$", ErrorMessage = "Postal code must be 4 digits")]
         public string PostalCode { get; set; }
     }
 }
